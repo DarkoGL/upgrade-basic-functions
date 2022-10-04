a701b9743141ad0ed90e548506daf9bb3637a3a3
Adolfo Gomez <agomezligero@yahoo.es>
2022-10-04 11:28:01 +0200

    Iteración #1 terminada

---
+++ 01-buscar-maximo.js
@@ -0,0 +1,14 @@
+// Iteración #1: Buscar el máximo
+// Completa la función que tomando dos números como argumento devuelva el más alto.
+
+function sum(numberOne , numberTwo) {
+    if(numberOne > numberTwo) return numberOne
+    else if(numberOne < numberTwo) return numberTwo
+    else return ("Los números son iguales");
+  }
+
+  console.log(sum(9, 5));
+  console.log(sum(3, 5));
+  console.log(sum(5, 5));
+
+  
